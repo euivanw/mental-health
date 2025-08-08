71733d4991f920d5e7ce55ffd9f3cc3250c8f918
Ivan Wilhelm <contato@ivanwilhelm.dev>
2025-08-08 09:09:47 -0300

    chore: Initialize project.

---
+++ src/main/java/dev/ivanwilhelm/mental/health/MentalHealthApplication.java
@@ -0,0 +1,13 @@
+package dev.ivanwilhelm.mental.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentalHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ivanwilhelm/mental/health/MentalHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ivanwilhelm.mental.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
